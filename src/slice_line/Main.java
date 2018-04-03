89d324a29f01a7b5fb81a91dfd1902bfcd57d10a
andrewsouthard1 <andrew.southard@datadoghq.com>
2018-04-03 11:06:04 -0400

    Added print statement

--- src/slice_line/Main.java
+++ src/slice_line/Main.java
@@ -2,6 +2,6 @@ package slice_line;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("SliceLine app coming soon");
+		System.out.println("SliceLine app");
 	}
 }
